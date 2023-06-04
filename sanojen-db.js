8dadbe211087f2dacd66e8074823ee0c3c26896f
flippy873 <98759821+flippy873@users.noreply.github.com>
2023-06-04 10:22:39 +0300

    Add files via upload

---
+++ sanojen-db.js
@@ -0,0 +1,46 @@
+let wordList = [
+    {
+        word: "koti",
+        hint: "Tärkeä paikka"
+    },
+    {
+        word: "parents",
+        hint: "tärkeät ihmiset (sana on englanniksi)"
+    },
+    {
+        word: "lintsi",
+        hint: "huvipuisto"
+    },
+    {
+        word: "png",
+        hint: "kuvan tiedostomuoto"
+    },
+    {
+        word: "candy",
+        hint: "Makeinen (sana on englanniksi)"
+    },
+    {
+        word: "Laiva",
+        hint: "kulkuväline maiden välillä"
+    },
+    {
+        word: "kirja",
+        hint: "Tätä käytetään lukemiseen"
+    },
+    {
+        word: "aurinko",
+        hint: "Se loistaa taivaalla ja antaa valoa"
+    },
+    {
+        word: "koira",
+        hint: "Se on ihmisen paras ystävä ja usein lemmikkieläin"
+    },
+    {
+        word: "Sininen",
+        hint: "Tämä on yksi sateenkaaren väreistä."
+    },
+    {
+        word: "beatch",
+        hint: "Tämä on paikka, jossa meri tai järvi kohtaa maan. (sana on englanniksi)"
+    },
+]
